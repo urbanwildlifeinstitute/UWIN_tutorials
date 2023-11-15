d3f677e3dd94353265aa3f894b93cb8712b94356
karivera2194 <karivera@uri.edu>
2023-11-15 11:02:25 -0600

    graphic size

--- tutorials/static occupancy/static_occ_test.R
+++ tutorials/static occupancy/static_occ_test.R
@@ -87,7 +87,7 @@ siteCovs <- raccoon_wk %>%
 
 # We should also examine our covariates of interest to see if they should be scaled
 hist(raccoon_wk$water)
-ggsave("water_hist.tiff", width = 6, height = 6)
+ggsave("water_hist.tiff", width = 4)
 ggplot(raccoon_wk, aes(x = water)) +
   geom_histogram() +
   theme_minimal() +
@@ -95,7 +95,7 @@ ggplot(raccoon_wk, aes(x = water)) +
   labs(x = "Proportion water", y = "Site count") 
   
 hist(raccoon_wk$forest)
-ggsave("forest_hist.tiff", width = 6, height = 6)
+ggsave("forest_hist.tiff", width = 4)
 ggplot(raccoon_wk, aes(x = forest)) +
   geom_histogram() +
   theme_minimal() +
