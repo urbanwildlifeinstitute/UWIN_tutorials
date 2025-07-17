d0d13d9d5029b15cfdc6c375686ea1402e99daa6
Kimberly Rivera <kimberly.rivera22194@gmail.com>
2025-07-17 02:05:55 -0500

    update git ignore

--- R.gitignore.R
+++ R.gitignore.R
@@ -5,5 +5,6 @@ static_occ_draft.R
 occ2_tutorial.R
 spatial-mapping_KR.R
 
-# Power points
 
+# Power points
+"C:/Users/krivera/OneDrive - Lincoln Park Zoo/Documents/GitHub/UWIN_tutorials/Mada_workshop/Test_folder"
